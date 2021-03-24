6dafdda2ef54ce972cc236708ceaedc49a6c81fc
Marouéne TAYARI <tayari_marouene@yahoo.fr>
2021-03-24 01:57:07 +0100

    Get Films from API

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -1,16 +1,24 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Film extends Model
-{
-    use HasFactory;
-    protected $fillable=['original_title','poster_path','overview','release_date',
-'genres','vote_average'];
-    public function Actors(){
-        $this->belongsToMany(Actor::class)->withPivot('title');
-    }
-}
+	
+	namespace App\Models;
+	
+	use Illuminate\Database\Eloquent\Factories\HasFactory;
+	use Illuminate\Database\Eloquent\Model;
+	
+	class Film extends Model
+	{
+		use HasFactory;
+		protected $fillable = [
+			'original_title',
+			'poster_path',
+			'overview',
+			'release_date',
+			'genres',
+			'vote_average'
+		];
+		
+		public function Actors()
+		{
+			$this->belongsToMany(Actor::class)->withPivot('title');
+		}
+	}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 Route::get('/eya', function () {
     return view('MovieSingle');
 });
+
